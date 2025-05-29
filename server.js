ece43f41d1639e0fc9b66679058371221f601234
Amp3rs4n <kisjlyk97@gmail.com>
2025-05-29 19:39:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Увімкнути CORS для всіх запитів
 app.use(cors());
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
+// Обробка HEAD і GET запитів на /ping
+app.route('/ping')
+  .get((req, res) => res.send('pong'))
+  .head((req, res) => res.status(200).end());
 
 app.listen(PORT, () => {
   console.log(`Ping server running at http://localhost:${PORT}`);
