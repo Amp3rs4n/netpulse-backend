7dfc4b82f30e16acf8d8b4f4296a5f133786fd61
Amp3rs4n <kisjlyk97@gmail.com>
2025-05-29 22:18:52 +0300

    Update empty.php

--- empty.php
+++ empty.php
@@ -1,5 +1,14 @@
 <?php
+// Дозволяємо CORS
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+// Якщо метод OPTIONS, просто завершити відповідь
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 header('HTTP/1.1 200 OK');
 
